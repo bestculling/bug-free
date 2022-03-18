d532a723f017a3e15f62fc94fcb5d343e7e455dc
Akkrapol Kanpong <aukkrapolarm@gmail.com>
2022-03-19 05:38:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "/script.js";
+        script.async = true;
+        script.onload = () => this.scriptLoaded();
+
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <div className="sc-bottom-bar">
+            <a className="sc-menu-item">
+                <i className="fas fa-list"></i>
+            </a>
+            <a class="sc-menu-item sc-current">
+                <i className="fas fa-plus"></i>
+            </a>
+            <a className="sc-nav-indicator"></a>
+            <a className="sc-menu-item">
+                <i className="fas fa-calendar-alt"></i>
+            </a>
+        </div>
+    );
 }
 
 export default App;
